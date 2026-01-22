b722f58e9a78e3f4e307396ebf2e821fdd045b3f
bella <cofla0904@naver.com>
2026-01-22 16:16:27 +0900

    chore: CI/CD 분리

---
+++ pm2/ecosystem.ai.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    apps: [
+        {
+            name: 'ai-service',
+            script: './venv/bin/uvicorn',
+            args: 'api.main:app --host 0.0.0.0 --port 8000',
+            cwd: '/home/ubuntu/refit/app/ai/ai_app',
+            interpreter: 'none',
+            instances: 1,
+            exec_mode: 'fork',
+            autorestart: true,
+            max_memory_restart: '500M',
+            env: {
+                PYTHONPATH: '/home/ubuntu/refit/app/ai/ai_app',
+                PORT: 8000,
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PYTHONPATH: '/home/ubuntu/refit/app/ai/ai_app',
+                PORT: 8000,
+            },
+            error_file: '/home/ubuntu/refit/logs/ai/error.log',
+            out_file: '/home/ubuntu/refit/logs/ai/out.log',
+            merge_logs: true,
+            log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        },
+    ],
+};
