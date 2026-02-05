6b9119e2aa8b9655c150000f783389f58673b59e
yseo14 <lmslys14@naver.com>
2026-02-05 16:13:20 +0900

    feat: Add EC2 IAM role/profile and attach to instance

--- terraform/dev/ec2.tf
+++ terraform/dev/ec2.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "main" {
   key_name               = var.key_name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2.name
 
   root_block_device {
     volume_size           = var.root_volume_size
